b22eb4a11d336dfd220fb6c46fd0455c78a41c30
Matheus Carvalho <55814214+matheuscarv69@users.noreply.github.com>
2021-10-31 15:27:14 -0300

    merge "develop" into "main" (#7)
    * :sparkles: feat: Worker Register function implemented
    
    * :sparkles: feat: Worker Update function implemented
    
    * :sparkles: feat: Get Worker By Id function implemented
    
    * :sparkles: feat: Get All Workers by Department
    
    * :sparkles: feat: Enable and Disable Worker fucntions implemented
    
    * :recycle: fix: adjust in functions name
    
    * :recycle: refactor: removing dependencies

--- src/main/kotlin/src/entities/worker/controller/WorkerController.kt
+++ src/main/kotlin/src/entities/worker/controller/WorkerController.kt
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
 import org.springframework.data.domain.Sort.Direction.ASC
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
